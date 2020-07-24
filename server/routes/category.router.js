16c69e887b28f2c0b47421336db5e260b2f3fe0f
“brunoreyes” <bruno619reyes@gmail.com>
2020-07-23 20:04:02 -0500

    attempted search view

--- server/routes/category.router.js
+++ server/routes/category.router.js
@@ -1,19 +1,48 @@
 const express = require('express');
 const pool = require('../modules/pool');
-
+const { default: Axios } = require('axios');
 const router = express.Router();
+require('dotenv').config();
 
-router.get('/', (req, res) => {
-    // return all categories
-    const queryText = `SELECT * FROM category ORDER BY name ASC`;
-    pool.query(queryText)
-        .then( (result) => {
-            res.send(result.rows);
-        })
-        .catch( (error) => {
-            console.log(`Error on query ${error}`);
-            res.sendStatus(500);
-        });
+// router.get('/', (req, res) => {
+//   // return all categories
+//   const queryText = `SELECT * FROM category ORDER BY name ASC`;
+//   pool
+//     .query(queryText)
+//     .then((result) => {
+//       res.send(result.rows);
+//     })
+//     .catch((error) => {
+//       console.log(`Error on query ${error}`);
+//       res.sendStatus(500);
+//     });
+// });
+
+// Go to Giphy to get searched images
+router.get('/search', (req, res) => {
+  console.log('Hit server to get searched gifs...', req.query.searchName);
+  console.log(
+    `this is the link`,
+    `http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&limit=5&rating=g&q=${req.query.searchName}`
+  );
+
+  // req.query.searchName is coming from the index.js
+  Axios.get(
+    `http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&limit=5&rating=g&q=${req.query.searchName}`
+  )
+    // api.giphy.com/v1/gifs/search?api_key=DCCQVL9Wnov2WkrU0Mzm3429Fy3ck0lQ&limit=5&rating=g&q=cheeseburger
+
+    .then((response) => {
+      // Remember 1st put in 'http://' then insert the rest of the link
+      //   res.sendStatus(200); //okay status
+      // you can't res.send twice so make sure to comment out sendStatus(200)
+      console.log('Sending back data:', response.data);
+      res.send(response.data.data);
+    })
+    .catch((error) => {
+      console.log('Error getting searched', error);
+      res.sendStatus(500); //not okay status
+    });
 });
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import App from './components/App/App.js';
+// import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// Provider allows us to use redux within our react app
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+// Create the rootSaga generator function
+function* rootSaga() {
+  yield takeEvery('FETCH_SEARCH', getSearchSaga);
+  //   yield takeEvery('SEND_FAVORITE', favoriteSearchSaga);
+  // yield takeEvery('DELETE_SEARCH', deleteSearchSaga);
+} //end rootSaga
+
+// Reducer that holds our results
+
+//this reducer holds the searched for name
+const searchName = (state = [], action) => {
+  if (action.type === 'SET_SEARCH') {
+    return action.payload;
+  }
+  return state;
+};
+
+const favoriteName = (state = [], action) => {
+  console.log('in favoriteName', action.payload);
+
+  if (action.type === 'SEND_FAVORITE') {
+    return action.payload;
+  }
+  return state;
+};
+
+// function* favoriteSearchSaga(action) {
+//   // here we are posting info to our database
+//   axios.get;
+// }
+
+function* getSearchSaga(action) {
+  try {
+    console.log('Searching with:', action.payload);
+
+    const response = yield axios.get(
+      `/api/category/search?searchName=${action.payload.name}`
+    );
+
+    // coming from search form
+    yield put({ type: 'SET_SEARCH', payload: response.data });
+  } catch (error) {
+    console.log('error with Search get request:', error);
+  } //end axios
+} //end getSearchSaga
+
+const sagaMiddleware = createSagaMiddleware();
+
+// Create one store that all components can use
+const storeInstance = createStore(
+  combineReducers({ searchName, favoriteName }),
+  // Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger)
+);
+
+// Pass rootSaga into our sagaMiddleware
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={storeInstance}>
+    <App />
+  </Provider>,
+  document.getElementById('react-root')
+  //   Change react to react-root
+);
+// registerServiceWorker();
